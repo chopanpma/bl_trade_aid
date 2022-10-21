68f501d5ae488e1d04a5b8105466c404a118c28b
Francisco Castaneda <francisco.castaneda@payjoy.com>
2022-10-21 09:08:51 -0500

    fix url

--- bl_trade_aid/patterns/apps.py
+++ bl_trade_aid/patterns/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PatternsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'patterns'
+    name = 'bl_trade_aid.patterns'
